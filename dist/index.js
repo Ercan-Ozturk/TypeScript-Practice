a7f8d49bd72604376c7f78f388a2f33021e61435
ercan-ozturk <ercanozturk788@gmail.com>
2024-09-06 12:03:35 +0300

    Practice

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,32 @@
 "use strict";
 let id = 5;
-console.log("ID:", id);
+let company = "Hello";
+let isDone = true;
+let x = 0.0;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, "hello"];
+let person = [1, "John", true];
+let employee;
+employee = [
+  [1, "John"],
+  [2, "Joe"],
+  [3, "Jane"],
+];
+let union_id = 30;
+var Direction1;
+(function (Direction1) {
+  Direction1[(Direction1["Up"] = 0)] = "Up";
+  Direction1[(Direction1["Down"] = 1)] = "Down";
+  Direction1[(Direction1["Left"] = 2)] = "Left";
+  Direction1[(Direction1["Right"] = 3)] = "Right";
+})(Direction1 || (Direction1 = {}));
+const user = {
+  id: 1,
+  name: "John",
+};
+let cid = 1;
+let customerId = cid;
+function addNum(x, y) {
+  return x + y;
+}
+console.log(addNum(5, 9));
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,43 @@
 let id: number = 5;
+let company: string = "Hello";
+let isDone: boolean = true;
+let x: any = 0.0;
 
-console.log("ID:", id);
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, true, "hello"];
+
+let person: [number, string, boolean] = [1, "John", true];
+
+let employee: [number, string][];
+
+employee = [
+  [1, "John"],
+  [2, "Joe"],
+  [3, "Jane"],
+];
+
+let union_id: number | string = 30;
+
+enum Direction1 {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+type User = {
+  id: number;
+  name: string;
+};
+const user: User = {
+  id: 1,
+  name: "John",
+};
+
+let cid: any = 1;
+let customerId = <number>cid;
+
+function addNum(x: number, y: number): number {
+  return x + y;
+}
+console.log(addNum(5, 9));
